45567bf416ebf7c89fd418045e484134481785e2
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2018-03-18 22:25:58 -0400

    initial commit

---
+++ scripts/PlayerShip.gd
@@ -0,0 +1,12 @@
+extends Area2D
+
+export(float) var health = 4.0
+export(Vector2) var velocity = Vector2()
+export(float) var firing_rate = 0.25
+
+func _ready():
+	
+	pass
+
+#func _process(delta):
+#	pass
