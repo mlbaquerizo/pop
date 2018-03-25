aa59d8109eac5fba5ed7bfdace7295db3b3f603c
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2018-03-25 19:48:10 -0400

    Refactor and update input logic using process_input

--- scripts/PlayerShip.gd
+++ scripts/PlayerShip.gd
@@ -13,37 +13,76 @@ var speed = 0
 func _ready():
 	screensize = get_viewport_rect().size
 	set_process(true)
+	set_process_input(true)
+	pass
+
+func _input(event):
+	var move_up = event.is_action_pressed("move_up")
+	var move_right = event.is_action_pressed("move_right")
+	var move_down = event.is_action_pressed("move_down")
+	var move_left = event.is_action_pressed("move_left")
+	
+	var input_move_up = Input.is_action_pressed("move_up")
+	var input_move_right = Input.is_action_pressed("move_right")
+	var input_move_down = Input.is_action_pressed("move_down")
+	var input_move_left = Input.is_action_pressed("move_left")
+	
+	var not_moving = !(input_move_up || input_move_right || input_move_down || input_move_left)
+	var moving = move_up || move_right || move_down || move_left
+	
+	if move_up:
+		if input_move_down:
+			self.input_direction.y = 0
+		else:
+			self.input_direction.y = -1
+		
+	if move_right:
+		if input_move_left:
+			self.input_direction.x = 0
+		else:
+			self.input_direction.x = 1
+	
+	if move_down:
+		if input_move_up:
+			self.input_direction.y = 0
+		else:
+			self.input_direction.y = 1
+	
+	if move_left:
+		if input_move_right:
+			self.input_direction.x = 0
+		else:
+			self.input_direction.x = -1
+	
+	if event.is_action_released("move_right"):
+		if input_move_left:
+			self.input_direction.x = -1
+	
+	if event.is_action_released("move_left"):
+		if input_move_right:
+			self.input_direction.x = 1
+	
+	if event.is_action_released("move_up"):
+		if input_move_down:
+			self.input_direction.y = 1
+	
+	if event.is_action_released("move_down"):
+		if input_move_up:
+			self.input_direction.y = -1
+	
+	if !(input_move_up || input_move_down):
+		self.input_direction.y = 0
+	
+	if !(input_move_right || input_move_left):
+		self.input_direction.x = 0
+		
+	if not_moving:
+		self.input_direction = Vector2()
+	
 	pass
 
 func _process(delta):
 	var velocity = Vector2()
-	
-	var move_up = Input.is_action_pressed("move_up")
-	var move_right = Input.is_action_pressed("move_right")
-	var move_down = Input.is_action_pressed("move_down")
-	var move_left = Input.is_action_pressed("move_left")
-	var not_moving = !(move_up || move_right|| move_down || move_left)
-	
-	if move_up && !move_down:
-		self.input_direction.y = -1
-	
-	if move_right && !move_left:
-		self.input_direction.x = 1
-	
-	if move_down && !move_up:
-		self.input_direction.y = 1
-	
-	if move_left && !move_right:
-		self.input_direction.x = -1
-	
-	if !(move_up || move_down):
-		self.input_direction.y = 0
-	
-	if !(move_right || move_left):
-		self.input_direction.x = 0
-	
-	if not_moving:
-		self.input_direction = Vector2()
 	
 	if self.input_direction.length() != 0:
 		self.speed += self.ACCELERATION * delta
@@ -51,8 +90,7 @@ func _process(delta):
 		self.speed -= self.DECELERATION * delta
 	
 	self.speed = clamp(self.speed, 0, self.MAX_SPEED)
-
-	velocity = self.input_direction.normalized() * self.speed
+	velocity += self.input_direction.normalized() * self.speed
 	
 	var new_pos = self.position
 	new_pos += velocity * delta
