973e3d9f05724a2a202e6cefc30139f9eb15019d
Matthew Baquerizo <mlbaquerizo47@gmail.com>
2018-03-26 21:11:14 -0400

    Revert "8 ship scene"

--- scripts/PlayerShip.gd
+++ scripts/PlayerShip.gd
@@ -1,118 +1,12 @@
 extends Area2D
 
-export(float) var health = 4.0 setget set_health
-export(float) var MAX_SPEED = 1000
-export(float) var ACCELERATION = 4000
-export(float) var DECELERATION = 800
+export(float) var health = 4.0
+export(Vector2) var velocity = Vector2()
 export(float) var firing_rate = 0.25
 
-var screensize
-var input_direction = Vector2()
-var speed = 0
-
 func _ready():
-	screensize = get_viewport_rect().size
-	set_process(true)
-	set_process_input(true)
-	pass
-
-func _input(event):
-	var move_up = event.is_action_pressed("move_up")
-	var move_right = event.is_action_pressed("move_right")
-	var move_down = event.is_action_pressed("move_down")
-	var move_left = event.is_action_pressed("move_left")
-	
-	var input_move_up = Input.is_action_pressed("move_up")
-	var input_move_right = Input.is_action_pressed("move_right")
-	var input_move_down = Input.is_action_pressed("move_down")
-	var input_move_left = Input.is_action_pressed("move_left")
-	
-	var not_moving = !(input_move_up || input_move_right || input_move_down || input_move_left)
-	var moving = move_up || move_right || move_down || move_left
-	
-	if move_up:
-		if input_move_down:
-			self.input_direction.y = 0
-		else:
-			self.input_direction.y = -1
-		
-	if move_right:
-		if input_move_left:
-			self.input_direction.x = 0
-		else:
-			self.input_direction.x = 1
-	
-	if move_down:
-		if input_move_up:
-			self.input_direction.y = 0
-		else:
-			self.input_direction.y = 1
-	
-	if move_left:
-		if input_move_right:
-			self.input_direction.x = 0
-		else:
-			self.input_direction.x = -1
-	
-	if event.is_action_released("move_right"):
-		if input_move_left:
-			self.input_direction.x = -1
-	
-	if event.is_action_released("move_left"):
-		if input_move_right:
-			self.input_direction.x = 1
-	
-	if event.is_action_released("move_up"):
-		if input_move_down:
-			self.input_direction.y = 1
-	
-	if event.is_action_released("move_down"):
-		if input_move_up:
-			self.input_direction.y = -1
-	
-	if !(input_move_up || input_move_down):
-		self.input_direction.y = 0
-	
-	if !(input_move_right || input_move_left):
-		self.input_direction.x = 0
-		
-	if not_moving:
-		self.input_direction = Vector2()
 	
 	pass
 
-func _process(delta):
-	var velocity = Vector2()
-	
-	if self.input_direction.length() != 0:
-		self.speed += self.ACCELERATION * delta
-	else:
-		self.speed -= self.DECELERATION * delta
-	
-	self.speed = clamp(self.speed, 0, self.MAX_SPEED)
-	velocity += self.input_direction.normalized() * self.speed
-	
-	var new_pos = self.position
-	new_pos += velocity * delta
-	new_pos.x = clamp(new_pos.x, 60, screensize.x - 60)
-	new_pos.y = clamp(new_pos.y, 60, screensize.y - 60)
-	
-	self.position = new_pos
-	
-	pass
-
-func tilt(direction):
-	# Play tilt animation for the correct direction
-	pass
-
-func shoot():
-	# Create a projectile
-	# Play shooting animation
-	pass
-
-func set_health(new_value):
-	health = new_value
-	
-	if health <= 0:
-		self.queue_free()
-	pass
+#func _process(delta):
+#	pass
